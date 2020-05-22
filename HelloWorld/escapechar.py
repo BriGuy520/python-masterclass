575ec225b68b952596af63d43fef86d459e0003b
Brian Falasz <brianfalasz@gmail.com>
2020-05-22 17:01:26 -0500

    adding python files

---
+++ HelloWorld/escapechar.py
@@ -0,0 +1,30 @@
+splitString = "This string has been\nsplit over\nseveral\nlines"
+print(splitString)
+
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
+#or
+print("the pet shop owner said \"No, no, 'e's uh,...he's resting\".")
+
+print("""The pet shop owner said "No, no, \
+'e's uh,...he's resting". """)
+
+anotherSplitString = """This string has been
+split over 
+several 
+lines"""
+
+print(anotherSplitString)
+
+anotherSplitStringOnSameLine = """This string has been \
+split over \
+several \
+lines"""
+
+print(anotherSplitStringOnSameLine)
+
+
+print("C:\\Users\\tBrian Falasz\\notes.txt")
+print(r"C:\Users\tBrian Falasz\notes.txt")
---
+++ HelloWorld/strings.py
@@ -0,0 +1,11 @@
+print("today is a good day to learn python")
+print('Python is fun')
+print("Python's string are easy to use")
+print('We can even include "quotes" in strings')
+print("Hello" + " world")
+greeting = "Hello"
+name = input("Please enter your name ")
+print(greeting + name)
+
+# If we want a space, we can add that too
+print(greeting + " " + name)
