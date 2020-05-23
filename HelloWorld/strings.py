543108d414b9eb0986df44d1c5fad9af357c3ea4
Brian Falasz <bfalasz@yahoo.com>
2020-05-22 19:02:26 -0500

    adding an operatorts file

--- HelloWorld/strings.py
+++ HelloWorld/strings.py
@@ -4,8 +4,20 @@ print("Python's string are easy to use")
 print('We can even include "quotes" in strings')
 print("Hello" + " world")
 greeting = "Hello"
-name = input("Please enter your name ")
+name = "Brian"
 print(greeting + name)
 
 # If we want a space, we can add that too
 print(greeting + " " + name)
+
+
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age_in_words = "2 years"
+print(name + " is " + age  + " years old.")
+print(type(age_in_words))
+
