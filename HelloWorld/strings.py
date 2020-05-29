e3895048c19f675f6e5a1a6f1b4098a3f8c9c3ac
Brian Falasz <brianfalasz@gmail.com>
2020-05-29 01:09:40 -0500

    learned more about slicing in python

--- HelloWorld/strings.py
+++ HelloWorld/strings.py
@@ -31,8 +31,41 @@ print(type(age_in_words))
 
 
 print(parrot[0:6]) #Norweg
+
 print(parrot[1:7])
 print(parrot[3:5])
 print(parrot[0:9])
 print(parrot[:9])
+# slice the word 'blue'
+print(parrot[10:14])
+print(parrot[10:])
 
+print(parrot[:6])
+print(parrot[6:])
+
+print(parrot[:6] + parrot[6:])
+print(parrot[:])
+
+
+letters = 'abcdefghijklmnopqrstuvwxyz'
+
+print(parrot[-4:2])
+print(parrot[-4:-2])
+print(parrot[-4:12])
+
+print(parrot[-14:-8])
+
+# steps in a slice
+
+football = "Chicago Bears"
+
+print(football[0:6:2])
+print(football[0:6:3])
+
+number = "9,223;372:036 854,775;807"
+
+seperators = number[1::4]
+print(seperators)
+
+values = "".join(char if char not in seperators else " " for char in number).split()
+print([int(val) for val in values])
