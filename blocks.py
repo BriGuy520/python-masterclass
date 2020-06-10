831c88d31d40a22a9e08c60c2b8da3c06d184cff
Brian Falasz <brianfalasz@gmail.com>
2020-06-10 00:38:37 -0500

    adding an else if statement to blocks file

--- blocks.py
+++ blocks.py
@@ -6,14 +6,24 @@ name = input("Please enter your name: ")
 age = int(input("What is your age, {0} ".format(name)))
 
 
-if age >= 18: 
-  print("Congratulations {}, you can vote for the lesser of two evils".format(name))
-  print("Please put an X in the box")
-else:
-  print("Sorry {}, you're going to have to wait ".format(name) + str(18 - age) + " more years to throw away your vote")
+# if age >= 18: 
+#   print("Congratulations {}, you can vote for the lesser of two evils".format(name))
+#   print("Please put an X in the box")
+# else:
+#   print("Sorry {}, you're going to have to wait ".format(name) + str(18 - age) + " more years to throw away your vote")
+
+# if age < 18:
+#     print("Sorry {}, you're going to have to wait ".format(name) + str(18 - age) + " more years to throw away your vote")
+# else:
+#   print("Congratulations {}, you can vote for the lesser of two evils".format(name))
+#   print("Please put an X in the box")
+
 
 if age < 18:
     print("Sorry {}, you're going to have to wait ".format(name) + str(18 - age) + " more years to throw away your vote")
+elif age == 900:
+  print("Sorry, Yoda, you die in Return of the Jedi")
 else:
   print("Congratulations {}, you can vote for the lesser of two evils".format(name))
   print("Please put an X in the box")
+
